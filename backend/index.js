d586a37e4b6c65f9546c28a3ecae6bc39361b330
ashishfargade <fargadeashish@gmail.com>
2024-03-31 03:01:26 +0530

    table

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
-import cors from 'cors;'
+import cors from 'cors'
 
 const app = express();
 
